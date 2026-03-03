d2f68b5c83949e2c1ec51e12f53a9227a640c327
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-03-03 18:04:46 +0300

    The OpenSoftware-World website has been updated. (Not completed)
    Signed-off-by: OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>

--- 404NotFound/404JS/404_not_found.js
+++ 404NotFound/404JS/404_not_found.js
@@ -8,19 +8,19 @@ window.onload = function() {
     if (lang.startsWith("en")) {
         ErrorTitle.innerHTML = "404 not found error";
         ErrorDescription.innerHTML = "The page you are looking for was not found or the page does not exist 404 not found error.";
-        MainPage.innerHTML = "LinuxUsersLinuxMint - Main page";
-        SupportPage.innerHTML = "LinuxUsersLinuxMint - Support page";
-        console.error("LinuxUsersLinuxMint: 404 not found error. Sorry! The page or content you are trying to search for on this website may have been moved or deleted...");
+        MainPage.innerHTML = "OpenSoftware-World - Main page";
+        SupportPage.innerHTML = "OpenSoftware-World - Support page";
+        console.error("OpenSoftware-World: 404 not found error. Sorry! The page or content you are trying to search for on this website may have been moved or deleted...");
         alert("Sorry! The page or content you are looking for on this website may have been moved or deleted... Error code: 404");
-        document.title = "LinuxUsersLinuxMint - 404 Not found";
+        document.title = "OpenSoftware-World - 404 Not found";
     }
     else if (lang.startsWith("tr")) {
         ErrorTitle.innerHTML = "404 bulunamadı hatası";
         ErrorDescription.innerHTML = "Aradığınız sayfa bulunamadı veya sayfa mevcut değil 404 bulunamadı hatası.";
-        MainPage.innerHTML = "LinuxUsersLinuxMint - Ana sayfa";
-        SupportPage.innerHTML = "LinuxUsersLinuxMint - Destek sayfası";
-        console.error("LinuxUsersLinuxMint: 404 bulunamadı hatası. Üzgünüm! bu web sitesinde aramaya çalıştığınız sayfa veya içerik taşınmış veya silinmiş olabilir...");
+        MainPage.innerHTML = "OpenSoftware-World - Ana sayfa";
+        SupportPage.innerHTML = "OpenSoftware-World - Destek sayfası";
+        console.error("OpenSoftware-World: 404 bulunamadı hatası. Üzgünüm! bu web sitesinde aramaya çalıştığınız sayfa veya içerik taşınmış veya silinmiş olabilir...");
         alert("Üzgünüm! bu web sitesinde aramaya çalıştığınız sayfa veya içerik taşınmış veya silinmiş olabilir... Hata kodu: 404");
-        document.title = "LinuxUsersLinuxMint - 404 bulunamadı";
+        document.title = "OpenSoftware-World - 404 bulunamadı";
     }
 };
